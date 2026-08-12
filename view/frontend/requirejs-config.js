610fc8cb2e308f27fc87f94966df09d1330caf6d
unopuntozero <59834895+unopuntozero@users.noreply.github.com>
2026-08-12 05:52:40 +0200

    Add RequireJS mixin configuration

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'StripeIntegration_Payments/js/view/payment/method-renderer/stripe_payments': {
+                'Scidigital_StripeCheckoutUx/js/stripe-autoselect-mixin': true
+            }
+        }
+    }
+};
